17eb003fcaa44d062657ad49108d9fb178b0f7f0
Abhay <iamfedup8@gmail.com>
2021-08-14 00:15:39 +0530

    First Commit

--- changelog.md
+++ changelog.md
@@ -1,6 +1,7 @@
 # Change Log
 
-
+### 13-Aug-2021 : extract_data and (new)load_data_mono(incomplete), Abhay
+Added code to 'extract_data' to store files of a certain instrument into a list, and shuffle them. Added a new function load_data_mono in order to work with mono music instead of stereo - a decision we will have to make. The code is still incomplete, have to decide on what exactly needs to be done - in what format Wavenet takes input. 
 ### 12-Aug-2021 : initial files, Balan
 Added the `input.py` and `train.py` files with a rudimentary structure of functions to be implemented. More functions can be added if necessary. Defined classes for two models - VAE and WaveNet, to be implemented later. Download the IRMAS dataset from its mirror at [zenodo](https://zenodo.org/record/1290750#.YRQW1XUzbmx) (only the training set).
 
--- input.py
+++ input.py
@@ -2,11 +2,22 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import pyaudio
 import os
+import librosa
+import random
+
 
 
 def extract_data():
     # go through the files and pick out the right directory to use, shuffling, all that messing around
-    pass
+    path = 'G:\My Drive\Music Generation\IRMAS-TrainingData\cel'
+    #Using a placeholder path that consists of (mainly) a single instrument to avoid the complexity of polyphonic music. Will have to be decided upon later.
+    file_names = []
+    for file in os.listdir(path):
+        file_names.append(path + '\\' + file)
+    random.shuffle(file_names)
+    #Technically redundant, since the file names are appended in an arbitrary order
+    return file_names
+    #TODO:    Decide whether we want to use a single instrument. 
 
 def load_audio(directory_list):
     # use decode_wav from the audio submodule to read *.wav files
@@ -23,3 +34,14 @@ def visualize_waveform(wave):
     # given a waveforrm plot it. if given a list of waveforms, then use subplots
     pass
 
+def load_audio_mono(file_list):
+    # In case we are using mono music (1 channel) instead of stereo  (2 channels), since it is easier, and the vector size is halved.
+    tensor_list = []
+    for song in file_list:
+        x, sr = librosa.load(song, sr=22050) 
+        tensor_list.append[x]
+    # TODO: Split each tensor into smaller samples? The audio clips themselves are only 3 seconds long, so there is barely any temporal dependency.
+    #       Split into train,val,test - if using a certain instrument(?) it can be done manually, otherwise train_test_split twice.
+    #       Make use of tf.data.Datasets for convenience in batching and the like.
+    #       Figure out what exactly we are passing to the conv layers and in what shape.       
+    pass
